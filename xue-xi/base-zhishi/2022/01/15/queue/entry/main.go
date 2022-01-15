a5ded091c5e154981f512eba41fd3002bda6d5b0
songyanannba <1499067030@qq.com>
2022-01-16 00:34:12 +0800

    包的封装，关于队列

--- xue-xi/base-zhishi/2022/01/15/queue/entry/main.go
+++ xue-xi/base-zhishi/2022/01/15/queue/entry/main.go
@@ -1,1 +1,18 @@
-package entry
+package main
+
+import (
+	"fmt"
+	"go-2022/xue-xi/base-zhishi/2022/01/15/queue"
+)
+
+func main() {
+	q := queue.Queue{1}
+	q.Push(2)
+	q.Push(3)
+	fmt.Println(q.Pop())
+	fmt.Println(q.Pop())
+	fmt.Println(q.IsEmpty())
+	fmt.Println(q.Pop())
+	fmt.Println(q.IsEmpty())
+
+}
