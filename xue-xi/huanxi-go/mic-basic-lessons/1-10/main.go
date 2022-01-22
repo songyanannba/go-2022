51caf1d8e953669e32ea456040321de3dcd0aeb7
songyanannba <1499067030@qq.com>
2022-01-22 09:49:56 +0800

    数组

---
+++ xue-xi/huanxi-go/mic-basic-lessons/1-10/main.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+//数组
+func main() {
+	//1 定义 (一维)
+	var array1 [6]string
+	array2 := [3]string{"aa", "bb", "cc"}
+	array3 := [...]string{"vv", "bb"} //让编辑器处理几个
+
+	fmt.Println(array1)
+	fmt.Println(array2)
+	fmt.Println(array3)
+
+	//2 定义 (二维)
+	var matrix1 [3][4]string
+	var matrix2 [3][4]int
+
+	fmt.Println(matrix1)
+	fmt.Println(matrix2)
+
+	//3 遍历打印数组
+	for i := 0; i < len(array2); i++ {
+		fmt.Println(array2[i])
+	}
+
+	for index, item := range array2 {
+		fmt.Println(index, item)
+	}
+
+}
