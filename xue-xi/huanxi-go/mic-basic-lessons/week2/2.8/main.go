f691980f34edfdea38b5a0d5f74ea511bd676bff
songyanannba <1499067030@qq.com>
2022-01-28 17:14:01 +0800

    defer

---
+++ xue-xi/huanxi-go/mic-basic-lessons/week2/2.8/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func Cook() {
+	//defer 先进后出 栈 （子弹上膛）
+	//为什么要defer 1 防止内存溢出
+	defer fmt.Println("开饭")
+	defer fmt.Println("播放音乐")
+	fmt.Println("买菜")
+	fmt.Println("卖肉")
+	//panic("停水了")
+	fmt.Println("做饭")
+	fmt.Println("sss")
+}
+
+func WriteMee(fileName string, foods []string) {
+	curDir, _ := os.Getwd()
+	fmt.Println(curDir)
+	path := curDir + fileName
+	f, err := os.Create(path)
+	defer f.Close()
+	if err != nil {
+		panic(err)
+	}
+	w := bufio.NewWriter(f)
+	defer w.Flush()
+	for _, item := range foods {
+		fmt.Fprintln(w, item)
+	}
+}
+
+func main() {
+	//defer
+	//Cook()
+	foods := []string{"烧鸡", "烤鸭", "羊腿", "干炒牛肉"}
+	p := "/xue-xi/huanxi-go/mic-basic-lessons/week2/2.8/foods.txt"
+	WriteMee(p, foods)
+}
