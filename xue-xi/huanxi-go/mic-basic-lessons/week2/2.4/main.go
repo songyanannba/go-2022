7f6c867245f9114b3c8858c689b51cd0457d6aa4
songyanannba <1499067030@qq.com>
2022-01-28 14:38:31 +0800

    接口组合

---
+++ xue-xi/huanxi-go/mic-basic-lessons/week2/2.4/main.go
@@ -0,0 +1,55 @@
+package main
+
+import "fmt"
+
+type Run interface {
+	Running()
+}
+
+type Swim interface {
+	Swimming()
+}
+
+
+type Sport interface {
+	Run
+	Swim
+}
+
+func GoSport(s Sport) {
+	s.Running()
+	s.Swimming()
+}
+
+type BOY struct {
+	Name string
+}
+
+func (b *BOY) Running() {
+	fmt.Println("runing1")
+}
+
+type GIRL struct {
+	Name string
+}
+
+func (b *BOY) Swimming() {
+	fmt.Println("swimming2")
+}
+
+/*
+type Spo struct {
+	BOY
+	GIRL
+}*/
+
+/*func (s *Spo) Running() {
+	fmt.Println("Spo 33")
+}*/
+
+func main() {
+
+	//s := Spo{}
+	b := BOY{}
+	GoSport(&b)
+}
