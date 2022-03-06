64af21d66396e627427e723ffd389dc03c5de218
songyanannba <1499067030@qq.com>
2022-03-06 16:45:01 +0800

    14

--- xue-xi/huanxi-go/mic-trainning-lessons-part2/product_web/main.go
+++ xue-xi/huanxi-go/mic-trainning-lessons-part2/product_web/main.go
@@ -4,10 +4,14 @@ import (
 	"fmt"
 	"github.com/gin-gonic/gin"
 	"github.com/google/uuid"
+	"go.uber.org/zap"
 	"mic-training-lessons-part2/internal"
 	"mic-training-lessons-part2/internal/register"
 	"mic-training-lessons-part2/product_web/hander"
 	"mic-training-lessons-part2/util"
+	"os"
+	"os/signal"
+	"syscall"
 )
 
 var (
@@ -51,5 +55,25 @@ func main() {
 	}
 
 	r.GET("/health", hander.HealthHandler)
-	r.Run(addr)
+
+	go func() {
+		err := r.Run(addr)
+		if err != nil {
+			zap.S().Panic(addr + "启动失败" + err.Error())
+		} else {
+			zap.S().Info(addr + "启动成功")
+		}
+	}()
+	q := make(chan os.Signal)
+	signal.Notify(q, syscall.SIGINT, syscall.SIGTERM)
+	<-q
+
+	err := consulRegistry.DeRegister(randomId)
+
+	if err != nil {
+		zap.S().Panic("注销失败" + randomId + ":" + err.Error())
+	} else {
+		zap.S().Info("注销成功" + randomId)
+	}
+	//r.Run(addr)
 }
