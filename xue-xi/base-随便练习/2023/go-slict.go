5c445ae6c1069587ee8f90a2970a2effd3b60af6
songyanannba <1499067030@qq.com>
2023-05-05 17:54:13 +0800

    xx

--- xue-xi/base-随便练习/2023/go-slict.go
+++ xue-xi/base-随便练习/2023/go-slict.go
@@ -2,7 +2,23 @@ package main
 
 import "fmt"
 
+func intToBytes(n int) []byte {
+	buf := make([]byte, 3)
+	buf[0] = byte((n >> 16) & 0xFF)
+	buf[1] = byte((n >> 8) & 0xFF)
+	buf[2] = byte(n & 0xFF)
+	return buf
+}
+
 func main() {
+
+	fmt.Println(intToBytes(1))
+	fmt.Println(intToBytes(2))
+	fmt.Println( string(intToBytes(1)) )
+
+
+	return
+
 	var a []string
 	var b  = []string{}
 
